1025c0bbe0e4e2b96d125576369a95a32d612965
karlno <yuanyin1985@126.com>
2019-03-01 10:11:21 +0800

    添加注释

--- src/Core/Controllers/QComController.c
+++ src/Core/Controllers/QComController.c
@@ -3,9 +3,15 @@
 Q-Ctrl是一套基于事件的控制器框架，类比于MCV框架，Q-Ctrl用于协调
 存储（Data）、输入输出（IO）、控制器（Controller）三者的逻辑处理，
 简称DIC框架。
+
 Q-Ctrl基于stm32有大量的驱动代码可直接调用，也可以移植于其他单片机平台，
 无需操作系统的支持，在遵守控制器编程规则的情况下，可处理以往需要操作系统
 才能处理的复杂业务。
+
+所有基于酷享物联平台进行的开发或案例、产品，均可联系酷享团队，免费放置于
+酷物联视频（q-iot.cn）进行传播或有偿售卖，相应所得收入扣除税费及维护费用后，
+均全额提供给贡献者，以此鼓励国内开源事业。
+
 By Karlno 酷享科技
 
 本文件定义了一个控制器，本控制器用来解析qwifi通过com3返回的指令结果，
@@ -263,7 +269,7 @@ static EVENT_HANDLER_RESUTL QCom_Cmd_EF(EVENT_BIT_FLAG Event,int Len,const char 
 
 Finish:	
 	Q_Free(pBuf);
-	return EFR_OK;
+	return EFR_STOP;
 }
 
 //控制器定义和注册
